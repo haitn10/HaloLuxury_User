e01fff600d655a25287f00a98cb98e8228278415
HaiTN12 <HaiTN12@fsoft.com>
2023-05-20 13:59:14 +0700

    [HaiTN] Fic bug folders

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,7 +3,7 @@ import "./AppRouter.css";
 import HomePage from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Shop from "../pages/Shop";
+import Products from "../pages/Products";
 import ProductDetails from "../pages/ProductDetails";
 
 function AppRouter() {
@@ -11,12 +11,11 @@ function AppRouter() {
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/products" element={<Shop />} />
           <Route path="/*" element={<Navigate to="/" />} exact />
           <Route path="/" element={<HomePage />} index />
-          <Route path="/products" element={<Shop />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/products" element={<Products />} />
           <Route path="/products/:id/details" element={<ProductDetails />} />
         </Routes>
       </BrowserRouter>
