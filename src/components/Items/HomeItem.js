ee1eb02986029f6cb268f5633d6781cc95d7a291
HaiTN12 <HaiTN12@fsoft.com>
2023-05-23 09:23:55 +0700

    [HaiTN] Responsive UI Homepage

--- src/components/Items/HomeItem.js
+++ src/components/Items/HomeItem.js
@@ -3,17 +3,17 @@ import React from "react";
 const HomeItem = ({ logo, storeName, productImg, productName, oldPrice, newPrice }) => {
   return (
     <div className="flex-none py-6 px-3">
-      <div className="flex flex-col max-w-xs max-h-450 border-2 p-4 border-primary rounded-20 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+      <div className="flex flex-col sm:w-60 md:w-80 max-h-450 border-2 p-4 border-primary rounded-20 hover:shadow-xl transition-shadow duration-300 ease-in-out">
         <div className="flex gap-3 items-center cursor-pointer">
           <img
             src={logo}
             alt="channel"
-            className="rounded-20 w-10 h-10 object-scale-down ml-5"
+            className="rounded-20 w-8 h-8 md:w-10 md:h-10 object-scale-down ml-5"
           />
           <h1 className="font-semibold">{storeName}</h1>
         </div>
-        <div className="max-h-275 border-2 rounded-20 border-second flex justify-center mt-2 cursor-pointer hover:bg-second hover:bg-opacity-25 transition-colors ease-linear">
-          <img src={productImg} alt="" className="h-275 object-scale-down" />
+        <div className="max-h-195 md:max-h-275 border-2 rounded-20 border-second flex justify-center mt-2 cursor-pointer hover:bg-second hover:bg-opacity-25 transition-colors ease-linear">
+          <img src={productImg} alt="" className="h-150 md:h-275 object-scale-down" />
         </div>
         <div className="flex mt-3 p-3">
           <div className="w-auto mr-4 h-20 text-ellipsis overflow-hidden">
@@ -22,7 +22,7 @@ const HomeItem = ({ logo, storeName, productImg, productName, oldPrice, newPrice
             </p>
           </div>
           <div className="flex flex-col items-end">
-            <h1 className="font-semibold text-xl">${newPrice}</h1>
+            <h1 className="font-semibold md:text-xl">${newPrice}</h1>
             <h2 className="font-normal text-sm line-through text-red">${oldPrice}</h2>
           </div>
         </div>
--- src/components/Items/ServiceItem.js
+++ src/components/Items/ServiceItem.js
@@ -3,8 +3,8 @@ import React from "react";
 const ServiceItem = ({image, text}) => {
   return (
     <div className="flex flex-col items-center">
-      <img src={image} alt="" className="w-150" />
-      <h1 className="font-medium text-xl">{text}</h1>
+      <img src={image} alt="" className="w-20 md:w-150" />
+      <h1 className="font-medium md:text-xl">{text}</h1>
     </div>
   );
 };
